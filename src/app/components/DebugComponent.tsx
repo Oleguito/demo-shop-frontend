41d9cbe3f07f6d3abaceae13235b37fa258901d3
Oleguito <tenzoriator@rambler.ru>
2024-03-22 18:19:11 +0300

    while it works...

---
+++ src/app/components/DebugComponent.tsx
@@ -0,0 +1,35 @@
+import React, {useContext, useEffect, useState} from 'react';
+import {GlobalContext} from '../context/GlobalContext';
+import {MainLayout} from '@/app/components/MainLayout';
+import Cookies from "js-cookie";
+import {Header} from "@/app/components/Header";
+import * as cookies from "@/constants/cookies";
+
+
+const PageCarcass = () => {
+
+    const {loggedInUserData} = useContext(GlobalContext);
+    const {userIsLoggedIn} = useContext(GlobalContext);
+
+    const currentUserId = Cookies.get(cookies.currentUserID);
+    const userIsLoggedInCookies = currentUserId !== undefined;
+
+    const [isClient, setIsClient] = useState(false)
+    useEffect(() => {
+        setIsClient(true)
+    }, [currentUserId])
+
+    return (
+        <>
+            <div>
+                User Id (Cookies): <span>{isClient && currentUserId}</span>
+                <br/>
+                User is logged in (context): {userIsLoggedIn ? "yes" : "no"}
+                <br/>
+                User data (context): {JSON.stringify(loggedInUserData)}
+            </div>
+        </>
+    );
+};
+
+export default PageCarcass;
