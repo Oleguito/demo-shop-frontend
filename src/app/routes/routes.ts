2bdc5220c0de9947c52760fd6fe69f1d8f90d3b5
Oleguito <tenzoriator@rambler.ru>
2024-03-26 17:14:32 +0300

    while it works...

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,5 +3,5 @@ export const serverUrl = "http://localhost:8080";
 export const routes = {
     serverUrl: "http://localhost:8080",
     addUser: serverUrl + "/users/add",
-    register: serverUrl + "/pages/register",
+    register: serverUrl + "/register",
 }
