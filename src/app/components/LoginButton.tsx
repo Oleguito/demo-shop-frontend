eb13cc87c9329fe9e05f0d2e38c272d3dea7dc38
Oleguito <tenzoriator@rambler.ru>
2024-03-27 17:12:01 +0300

    while it works...

--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -1,15 +1,22 @@
-import React, {useContext} from "react";
-import {GlobalContext} from "@/app/context/GlobalContext";
+import React, { useContext } from 'react';
+import { GlobalContext } from '@/app/context/GlobalContext';
+import LoginOverlay from "@/app/components/LoginOverlay.tsx";
 
 export function LoginButton() {
-    const {loginOverlayVisible, setLoginOverlayVisible} = useContext(GlobalContext);
+    const { loginOverlayVisible, setLoginOverlayVisible } =
+        useContext(GlobalContext);
 
-    return <button
-        type="submit"
-        className="loginImage"
-        onClick={() => {
-            setLoginOverlayVisible(true);
-            console.log("Logging in...");
-        }}
-    ></button>;
+    return (
+        <>
+            <button
+                type="submit"
+                className="loginImage"
+                onClick={() => {
+                    setLoginOverlayVisible(true);
+                    console.log('Logging in...');
+                }}
+            ></button>
+            <LoginOverlay />
+        </>
+    );
 }
