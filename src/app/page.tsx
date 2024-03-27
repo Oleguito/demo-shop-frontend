40b6883d639850be7ef991b18617ef18ff2f421d
Oleguito <tenzoriator@rambler.ru>
2024-03-27 17:14:42 +0300

    while it works...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
             <main>
                 <DebugComponent />
                 <PageCarcass />
-                <LoginOverlay />
             </main>
         </>
     );
