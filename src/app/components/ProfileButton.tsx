2b6518fa691eed4c44527c76424a3334b1eebb7c
Oleguito <tenzoriator@rambler.ru>
2024-03-27 15:43:37 +0300

    while it works...

--- src/app/components/ProfileButton.tsx
+++ src/app/components/ProfileButton.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import {useRouter} from "next/navigation";
-import {frontend} from "@/app/routes/routes.ts";
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { frontend } from '@/app/routes/routes.ts';
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -8,38 +8,44 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {Button} from "@/components/ui/button.tsx";
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button.tsx';
 
 export default function ProfileButton() {
-
     const router = useRouter();
 
-    return <>
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Button
-                    type="submit"
-                    className="profileImage"
-                    onClick={() => {
-                        console.log("this is a profile button");
-                        router.push(frontend.profilePage);
-                    }}
-                ></Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator/>
-                <DropdownMenuItem onClick={() => {
-                    console.log("whatever")
-                }}>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-
-    </>
-
-
+    return (
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Button
+                        type="submit"
+                        className="profileImage"
+                        onClick={() => {
+                            console.log('this is a profile button');
+                            router.push(frontend.profilePage);
+                        }}
+                    ></Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem
+                        onClick={() => {
+                            console.log('whatever');
+                        }}
+                    >
+                        Profile
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        onClick={() => {
+                            console.log('Logging out...');
+                        }}
+                    >
+                        Log Out
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    );
 }
