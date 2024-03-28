304a9c52e281cbadf99a6f59956f6b04da7c6127
Oleguito <tenzoriator@rambler.ru>
2024-03-28 14:27:42 +0300

    while it works...

--- src/app/components/CreateCategoryButton.tsx
+++ src/app/components/CreateCategoryButton.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Button } from '@/components/ui/button.tsx';
+import {useRouter} from "next/navigation";
 
 const CreateCategoryButton = () => {
+
+    const router = useRouter();
+
     return (
         <>
-            <Button className={'bg-blue-950 hover:text-amber-100'}>Create Category</Button>
+            <Button className={'bg-blue-950'} onClick={() => {
+                console.log("hello");
+                router.push("/categories/create")
+            }}>
+                Create Category
+            </Button>
         </>
     );
 };
